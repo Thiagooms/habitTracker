6de6fd9dd57be4141c7d1a5f49e7ae2d9e901b73
Thiago <thiagopaulista50@gmail.com>
2025-08-27 08:50:04 -0300

    feat: Folder architecture created

---
+++ src/main/java/com/habitTracker/ArchitectureClean/ArchitectureCleanApplication.java
@@ -0,0 +1,13 @@
+package com.habitTracker.ArchitectureClean;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArchitectureCleanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArchitectureCleanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/habitTracker/ArchitectureClean/ArchitectureCleanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.habitTracker.ArchitectureClean;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArchitectureCleanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
